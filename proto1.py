ac4b7a4976a9b4b0cc7bf0dccb7f59ebcfe20141
Ap1075 <armaanpuri17@gmail.com>
2018-05-18 03:19:05 +0530

    Training done, next step Tuning

--- proto1.py
+++ proto1.py
@@ -133,7 +133,7 @@ def encode_output(sequences, vocab_size):
     ylist = list()
     for sequence in sequences:
         encoded = to_categorical(sequence, num_classes=vocab_size)
-		 ylist.append(encoded)
+        ylist.append(encoded)
     y = np.array(ylist)
     y = y.reshape(sequences.shape[0], sequences.shape[1], vocab_size)
     return y
@@ -142,15 +142,51 @@ def encode_output(sequences, vocab_size):
 def define_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
     model = Sequential()
     # encoder =>
-    model.add(Embedding(src_vocab, n_units, input_length=src_timesteps, mask_zero=True))
-    model.add(Bidirectional(LSTM(n_units)))
-    model.add(Bidirectional(LSTM(n_units*2)))
-    model.add(RepeatVector(tar_timesteps))
+    model.add(Embedding(src_vocab, n_units, input_length=src_timesteps, mask_zero=True)) #check its output and shape
+    model.add(Bidirectional(LSTM(n_units,return_sequences=True))) # uses previous output as input and performs computation on it.
+    model.add(Bidirectional(LSTM(n_units))) # tweak n_units
+    model.add(RepeatVector(tar_timesteps)) 
     #decoder =>
+    model.add(LSTM(n_units,return_sequences=True)) # +1 LSTM layer for decoding
     model.add(LSTM(n_units, return_sequences=True))
     model.add(TimeDistributed(Dense(tar_vocab, activation='softmax')))
     return model
  
+# prepare english tokenizer
+eng_tokenizer = create_tokenizer(dataset[:, 0])
+eng_vocab_size = len(eng_tokenizer.word_index) + 1
+eng_length = max_length(dataset[:, 0])
+print('English Vocabulary Size: {}'.format(eng_vocab_size))
+print('English Max Length: %d' % (eng_length))
+# prepare german tokenizer
+ger_tokenizer = create_tokenizer(dataset[:, 1])
+ger_vocab_size = len(ger_tokenizer.word_index) + 1
+ger_length = max_length(dataset[:, 1])
+print('German Vocabulary Size: %d' % ger_vocab_size)
+print('German Max Length: %d' % (ger_length))
 
+# prepare training data
+trainX = encode_sequences(ger_tokenizer, ger_length, train[:, 1])
+trainY = encode_sequences(eng_tokenizer, eng_length, train[:, 0])
+trainY = encode_output(trainY, eng_vocab_size)
+# prepare validation data
+testX = encode_sequences(ger_tokenizer, ger_length, test[:, 1])
+testY = encode_sequences(eng_tokenizer, eng_length, test[:, 0])
+testY = encode_output(testY, eng_vocab_size)
+
+# define model
+model = define_model(ger_vocab_size, eng_vocab_size, ger_length, eng_length, 256) # vocab size is total number of words, length is max sentence length
+model.compile(optimizer='adam', loss='categorical_crossentropy')
+
+# summarize defined model
+print(model.summary())
+plot_model(model, to_file='model.png', show_shapes=True)
+
+#fitting the model
+filename = "model1.h5"
+checkpt = ModelCheckpoint(filename, monitor = "val_loss", verbose = 1, save_best_only = True, mode="min")
+model.fit(trainX, trainY, epochs= 50, batch_size = 64, validation_data = (testX,testY), callbacks = [checkpt], verbose = 2)
+              
+        
 
 # Inference
