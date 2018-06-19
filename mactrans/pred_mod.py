a813642d7d325c6aeb90bd22bde4260cf21b46a5
Armaan Puri <armaanpuri17@gmail.com>
2018-06-19 00:41:33 -0700

    Modified accodrding to pep8.

--- mactrans/pred_mod.py
+++ mactrans/pred_mod.py
@@ -2,69 +2,70 @@ from keras.models import load_model
 from numpy import argmax
 from keras.preprocessing.sequence import pad_sequences
 import numpy as np
-#import tensorflow as tf
+# import tensorflow as tf
 import string
-#from keras.preprocessing.text import Tokenizer
+# from keras.preprocessing.text import Tokenizer
+
 
 class predicter(object):
-    
-    def __init__(self,src_sent,eng_tok_path,ger_tok_path,dict_vars_path):
-        self.src_sent=src_sent
-        self.eng_tok_path=eng_tok_path
-        self.ger_tok_path=ger_tok_path                                        
+
+    def __init__(self, src_sent, eng_tok_path,
+                 ger_tok_path, dict_vars_path):
+        self.src_sent = src_sent
+        self.eng_tok_path = eng_tok_path
+        self.ger_tok_path = ger_tok_path
         self.dict_vars_path = dict_vars_path
-        
-    #with tf.device("/gpu:0"):
-    def encode_sequences(self,tokenizer, length, lines):
-    	# integer encode sequences
+
+# with tf.device("/gpu:0"):
+    def encode_sequences(self, tokenizer, length, lines):
+        # integer encode sequences
         X = tokenizer.texts_to_sequences(lines)
         # pad sequences with 0 values
         X = pad_sequences(X, maxlen=length, padding='post')
         return X
-        
-    def word_from_id(self,integer, tokenizer):
+
+    def word_from_id(self, integer, tokenizer):
         for word, i in tokenizer.word_index.items():
-            if integer==i:
+            if integer == i:
                 return word
         return None
-    
-    def predictions(self,tokenizer, source, model):
-        prediction = model.predict(source,verbose=0)[0]
+
+    def predictions(self, tokenizer, source, model):
+        prediction = model.predict(source, verbose=0)[0]
         indices = [argmax(vector) for vector in prediction]
-        output =[]    
+        output = []
         for i in indices:
-            word = self.word_from_id(i,tokenizer)
+            word = self.word_from_id(i, tokenizer)
             if word is None:
                 break
             output.append(word)
-        return " ".join(output) 
-    
-    def fin_pred_model(self,sources,tokenizer,model):
-        for i,source in enumerate(sources):
-    #        source = np.array(source)
-            source = source.reshape((1,source.shape[0]))
+        return " ".join(output)
+
+    def fin_pred_model(self, sources, tokenizer, model):
+        for i, source in enumerate(sources):
+            source = source.reshape((1, source.shape[0]))
             translation = self.predictions(tokenizer, source, model)
-        print (translation)
-    
+        print(translation)
+
     def punc_remover_lower(line):
-        cleaned=[]    
-        punc_rem = str.maketrans("","",string.punctuation)
-        line =line.split()
-        line = [word.translate(punc_rem) for word in line]   
+        cleaned = []
+        punc_rem = str.maketrans("", "", string.punctuation)
+        line = line.split()
+        line = [word.translate(punc_rem) for word in line]
         line = [word.lower() for word in line]
         cleaned.append(' '.join(line))
         return cleaned
 
-        
     def execute(self):
-        eng_tokenizer = np.load(open(self.eng_tok_path,"rb"))
-        ger_tokenizer = np.load(open(self.ger_tok_path,"rb"))
-        dict_vars = np.load(open(self.dict_vars_path,'rb'))
-        
-        model = load_model(dict_vars["model_path"])        
-        
+        eng_tokenizer = np.load(open(self.eng_tok_path, "rb"))
+        ger_tokenizer = np.load(open(self.ger_tok_path, "rb"))
+        dict_vars = np.load(open(self.dict_vars_path, 'rb'))
+
+        model = load_model(dict_vars["model_path"])
+
         ger_sent = self.src_sent
         ger_sent = self.punc_remover_lower(ger_sent)
-        ger_sent = self.encode_sequences(ger_tokenizer, dict_vars['ger_maxlen'], ger_sent)
-        self.fin_pred_model(ger_sent, eng_tokenizer, model)
+        ger_sent = self.encode_sequences(ger_tokenizer,
+                                         dict_vars['ger_maxlen'], ger_sent)
 
+        self.fin_pred_model(ger_sent, eng_tokenizer, model)
--- mactrans/train_mod.py
+++ mactrans/train_mod.py
@@ -1,14 +1,14 @@
 import numpy as np
-#import re
-import string 
+# import re
+import string
 import pickle
-#from unicodedata import normalize
-#import matplotlib.pyplot as plt
-#import argparse
+# from unicodedata import normalize
+# import matplotlib.pyplot as plt
+# import argparse
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
-#from keras.utils.vis_utils import plot_model
+# from keras.utils.vis_utils import plot_model
 from keras.models import Sequential
 from keras.layers import LSTM
 from keras.layers import Dense
@@ -17,173 +17,166 @@ from keras.layers import RepeatVector
 from keras.layers import Bidirectional
 from keras.layers import TimeDistributed
 from keras.callbacks import ModelCheckpoint
-#from keras_attention import AttentionDecoder
+# from keras_attention import AttentionDecoder
 from keras.layers import Dropout
-#import keras
+# import keras
 import tensorflow as tf
 from keras import backend as K
 
+
 class trainer(object):
 
-    def __init__(self,model_path,word_vec_path,dataset_path):
+    def __init__(self, model_path, word_vec_path, dataset_path):
         self.model_path = model_path
         self.word_vec_path = word_vec_path
-        self.dataset_path=dataset_path
-                
+        self.dataset_path = dataset_path
+
     def load_doc(self):
         file = open(self.dataset_path, mode="rt", encoding="utf-8")
         text = file.read()
         file.close()
         return text
 
-#separate eng and ger
-    def to_pairs(self,file):
+# separate eng and ger
+    def to_pairs(self, file):
         lines = file.strip().split('\n')
-        pairs = [line.split('\t') for line in lines] 
+        pairs = [line.split('\t') for line in lines]
         return pairs
-    
+
     # normalizing the pairs
-    def norm_pairs(self,lines):
-        cleaned=[]
-        #    re_print = re.compile("[^{}]".format(re.escape(string.printable)))  #add escape seq(backslash) in front of everything except alphanumeric and _
-        punc = string.punctuation + "„" + "–"+"’"+'“'     
-        table = str.maketrans("","",punc)                     # mapping punctuations to None, hence, removing them
+    def norm_pairs(self, lines):
+        cleaned = []
+        punc = string.punctuation + "„" + "–"+"’"+'“'
+        table = str.maketrans("", "", punc)
         for pair in lines:
-            clean_pair=[]
+            clean_pair = []
             for line in pair:
-                # handling normalization of unicode chars
-        #            line = normalize("NFD",line).encode('ascii','ignore')
-        #            line = line.decode("UTF-8")
                 # tokenize on space
                 line = line.split()
                 # to lowercase
-        #            line = [word.lower() for word in line]
                 # remove punctuation here
                 line = [word.translate(table) for word in line]
                 line = [word.lower() for word in line]
-                # remiving non-printable chars
-        #            line = [re_print.sub('',w) for w in line]
-                #remove digits
-        #            line = [word for word in line if word.isalpha()]
                 # appending as string
                 clean_pair.append(" ".join(line))
             cleaned.append(clean_pair)
         return np.array(cleaned)
-        
-    def save_clean_data(self,sentences, filename):
-        pickle.dump(sentences, open(filename,'wb'))
+
+    def save_clean_data(self, sentences, filename):
+        pickle.dump(sentences, open(filename, 'wb'))
         print("saved: {}".format(filename))
-    
-    
-    def load_clean_dataset(self,filename):
+
+    def load_clean_dataset(self, filename):
         return np.load(open(filename, "rb"))
-        
-    #K.set_floatx("float16")
-     
+
+    # K.set_floatx("float16")
+
     # fit a tokenizer
-    def create_tokenizer(self,lines):
+    def create_tokenizer(self, lines):
         tokenizer = Tokenizer()
         tokenizer.fit_on_texts(lines)
         return tokenizer
-     
+
     # max sentence length
-    def max_length(self,lines):
+    def max_length(self, lines):
         return max(len(line.split()) for line in lines)
-     
+
     # encode and pad sequences
-    def encode_sequences(self,tokenizer, length, lines):
-    	# integer encode sequences
+    def encode_sequences(self, tokenizer, length, lines):
+        # integer encode sequences
         X = tokenizer.texts_to_sequences(lines)
         # pad sequences with 0 values
         X = pad_sequences(X, maxlen=length, padding='post')
         return X
-     
+
     # one hot encode target sequence using gen
-    def new_gen(self,trainX, trainY, batch_size, vocab_size):
+    def new_gen(self, trainX, trainY, batch_size, vocab_size):
         X = np.zeros((batch_size, trainX.shape[1]))
         Y = np.zeros((batch_size, trainY.shape[1], vocab_size))
         while (True):
             #        print("This is i {}".format(i))
-            for b in range(0,len(trainX),batch_size):
-                ylist=list()
-                if(b+batch_size>=len(trainX)):
+            for b in range(0, len(trainX), batch_size):
+                ylist = list()
+                if(b+batch_size >= len(trainX)):
                     break
                 X = trainX[b:b+batch_size]
-                for i in range(b,b+batch_size):
-                    encoded = to_categorical(trainY[i],num_classes=vocab_size) 
+                for i in range(b, b+batch_size):
+                    encoded = to_categorical(trainY[i],
+                                             num_classes=vocab_size)
                     ylist.append(encoded)
-                    Y=np.array(ylist)
-                    Y= Y.reshape(batch_size, trainY.shape[1], vocab_size)
-                yield X,Y
-                
-        
-    def read_all_embeddings(self,filename):
+                    Y = np.array(ylist)
+                    Y = Y.reshape(batch_size, trainY.shape[1], vocab_size)
+                yield X, Y
+
+    def read_all_embeddings(self, filename):
         embeddings = dict()
         with open(filename) as f:
             for line in f:
                 line = line.split()
-                if len(line)==65:            
-                    word = line[0]
-                    coeffs = line[1:]
-                    embeddings[word]= coeffs
+#                if len(line) == 65:
+                word = line[0]
+                coeffs = line[1:]
+                embeddings[word] = coeffs
         return embeddings
 
-    def mat_builder(self,embed,tokenizer,ger_voc):#ger_tokenizer
+    def mat_builder(self, embed, tokenizer, ger_voc):
         embedding_mat = np.zeros((ger_voc, 64))
-        count=0
-        for word,i in tokenizer.word_index.items():
+        count = 0
+        for word, i in tokenizer.word_index.items():
             embedding_vec = embed.get(word)
             if embedding_vec is not None:
-                embedding_mat[i]= embedding_vec
+                embedding_mat[i] = embedding_vec
             else:
-                count+=1
-        return embedding_mat,count
-                
-    
+                count += 1
+        return embedding_mat, count
+
     # bidirectional Encoder without attention
-    def define_model(self,src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units,embedding_mat):
-    #    gpu_opts = tf.GPUoptions(per_process_gpu_memory_fraction = 0.5)   ### handling the gpu allocator error
-    #    sess = tf.Session(config=tf.ConfigProto(gpu_options = gpu_opts))
-    
+    def define_model(self, src_vocab, tar_vocab, src_timesteps,
+                     tar_timesteps, n_units, embedding_mat):
         config = tf.ConfigProto()
-        config.gpu_options.allow_growth=True
+        config.gpu_options.allow_growth = True
         sess = tf.Session(config=config)
-        
+
         K.set_session(sess)
-        
+
         model = Sequential()
         # encoder =>
-        model.add(Embedding(src_vocab,embedding_mat.shape[1] , input_length=src_timesteps, mask_zero=True, weights=[embedding_mat], trainable=False)) #check its output and shape removed n_units
+        model.add(Embedding(src_vocab, embedding_mat.shape[1],
+                            input_length=src_timesteps, mask_zero=True,
+                            weights=[embedding_mat], trainable=False))
+
         model.add(Dropout(0.45))
-        model.add(Bidirectional(LSTM(n_units,return_sequences=True))) # uses previous output as input and performs computation on it.
+        model.add(Bidirectional(LSTM(n_units, return_sequences=True)))
         model.add(Dropout(0.45))
-        model.add(Bidirectional(LSTM(n_units))) # tweak n_units
-        model.add(RepeatVector(tar_timesteps)) 
-        #decoder =>           
+        model.add(Bidirectional(LSTM(n_units)))
+        model.add(RepeatVector(tar_timesteps))
+
+        # decoder =>
     #    model.add(AttentionDecoder(int(n_units/8),tar_vocab))
-        model.add(LSTM(n_units,return_sequences=True)) # +1 LSTM layer for decoding
-        model.add(LSTM(n_units,return_sequences=True))
-        model.add(LSTM(n_units,return_sequences=True))
+        model.add(LSTM(n_units, return_sequences=True))
+        model.add(LSTM(n_units, return_sequences=True))
+        model.add(LSTM(n_units, return_sequences=True))
         model.add(LSTM(n_units, return_sequences=True))
         model.add(TimeDistributed(Dense(tar_vocab, activation='softmax')))
-        
+
         return model
-    
-    def execute(self,no_units,batch_size,epochs,steps_per_epoch,val_steps):
-#        filename = self.dataset_path
+
+    def execute(self, no_units, batch_size, epochs,
+                steps_per_epoch, val_steps):
+        # filename = self.dataset_path
         doc = self.load_doc()
-        #splitting by languages
-        #lines = new_lines()
+        # splitting by languages
+        # lines = new_lines()
         pairs = self.to_pairs(doc)
-        #print("pairs %d"%len(pairs))
+        # print("pairs %d"%len(pairs))
         # cleaning
         cleaned_pairs = self.norm_pairs(pairs)
-        
+
         raw_dataset = cleaned_pairs
         np.random.shuffle(raw_dataset)
-        dataset = raw_dataset[:,:]
-        train, test = dataset[:135000],dataset[135000:]
-        
+        dataset = raw_dataset
+        train, test = dataset[:135000], dataset[135000:]
+
         eng_tokenizer = self.create_tokenizer(dataset[:, 0])
         eng_vocab_size = len(eng_tokenizer.word_index) + 1
         eng_length = self.max_length(dataset[:, 0])
@@ -191,11 +184,13 @@ class trainer(object):
         ger_tokenizer = self.create_tokenizer(dataset[:, 1])
         ger_vocab_size = len(ger_tokenizer.word_index) + 1
         ger_length = self.max_length(dataset[:, 1])
-        
-        self.save_clean_data(eng_tokenizer, "./output/eng_tok.pkl")    
-        self.save_clean_data(ger_tokenizer, "./output/ger_tok.pkl")    
-    
-        dict_vars = {"ger_maxlen": ger_length, "model_path":"./output/model_generator.h5","eng_maxlen":eng_length}
+
+        self.save_clean_data(eng_tokenizer, "./output/eng_tok.pkl")
+        self.save_clean_data(ger_tokenizer, "./output/ger_tok.pkl")
+
+        dict_vars = {"ger_maxlen": ger_length,
+                     "model_path": "./output/model_generator.h5",
+                     "eng_maxlen": eng_length}
         self.save_clean_data(dict_vars, "./output/dict_vars.pkl")
 
         trainX = self.encode_sequences(ger_tokenizer, ger_length, train[:, 1])
@@ -204,19 +199,31 @@ class trainer(object):
         # prepare validation data
         testX = self.encode_sequences(ger_tokenizer, ger_length, test[:, 1])
         testY = self.encode_sequences(eng_tokenizer, eng_length, test[:, 0])
-        
+
         embed = self.read_all_embeddings(self.word_vec_path)
 
-        embedding_mat,counter = self.mat_builder(embed,ger_tokenizer,ger_vocab_size)
-        
-        model = self.define_model(ger_vocab_size, eng_vocab_size, ger_length, eng_length, no_units) # vocab size is total number of words, length is max sentence length
-        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+        embedding_mat, counter = self.mat_builder(embed,
+                                                  ger_tokenizer,
+                                                  ger_vocab_size)
+
+        model = self.define_model(ger_vocab_size,
+                                  eng_vocab_size, ger_length,
+                                  eng_length, no_units)
+        model.compile(optimizer='adam',
+                      loss='categorical_crossentropy', metrics=['acc'])
 
         filegen = self.model_path
-        checkpt = ModelCheckpoint(filegen, monitor = "val_acc", verbose = 2, save_best_only = True, mode="max")
-        history=model.fit_generator(generator = self.new_gen(trainX,trainY, batch_size, eng_vocab_size),validation_data=self.new_gen(testX,testY,batch_size,eng_vocab_size), epochs=epochs,steps_per_epoch=steps_per_epoch, shuffle=False, validation_steps=val_steps , verbose=1, callbacks=[checkpt])
 
+        checkpt = ModelCheckpoint(filegen, monitor="val_acc",
+                                  verbose=2, save_best_only=True, mode="max")
 
+        generator = self.new_gen(trainX, trainY, batch_size, eng_vocab_size)
+        val_data = self.new_gen(testX, testY, batch_size, eng_vocab_size)
 
-
-        
+        history = model.fit_generator(generator=generator,
+                                      validation_data=val_data,
+                                      epochs=epochs,
+                                      steps_per_epoch=steps_per_epoch,
+                                      shuffle=False,
+                                      validation_steps=val_steps,
+                                      verbose=1, callbacks=[checkpt])
