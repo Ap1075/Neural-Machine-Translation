7638079672d6ca2aeeae5deeea7237185d1f3087
Armaan Puri <armaanpuri17@gmail.com>
2018-06-19 00:42:08 -0700

    Modified according to pep8

--- bin/predict_mt.py
+++ bin/predict_mt.py
@@ -1,18 +1,30 @@
 from mactrans.pred_mod import predicter
 import argparse
-#import tensorflow as tf
+# import tensorflow as tf
+
 
 def parser_creator():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", metavar="string_to_translate",dest="src_sent", help="This is the string that must be translated to the other language", type=str)
-    parser.add_argument("-e", metavar="Path to english tokenizer",dest="eng_tok_path", help="Path to the trained english tokenizer.", type=str)
-    parser.add_argument("-g", metavar="Path to german tokenizer",dest="ger_tok_path", help="Path to the trained german tokenizer.", type=str)
-    parser.add_argument("-d", metavar="Path to dict_vars",dest="dict_vars_path", help="Path to the dictionary containing variables to be used for prediction.", type=str)
+    parser.add_argument("-s", metavar="string_to_translate",
+                        dest="src_sent",
+                        help="String to be translated to other language",
+                        type=str)
+    parser.add_argument("-e", metavar="Path to english tokenizer",
+                        dest="eng_tok_path",
+                        help="Path to trained english tokenizer.", type=str)
+    parser.add_argument("-g", metavar="Path to german tokenizer",
+                        dest="ger_tok_path",
+                        help="Path to the trained german tokenizer.",
+                        type=str)
+    parser.add_argument("-d", metavar="Path to dict_vars",
+                        dest="dict_vars_path",
+                        help="Path to dict containing magic numbers.",
+                        type=str)
     return parser
-    
-if __name__=="__main__":
+
+if __name__ == "__main__":
     parser = parser_creator()
     args = parser.parse_args()
-    predict = predicter(args.src_sent,args.eng_tok_path,args.ger_tok_path,args.dict_vars_path)
+    predict = predicter(args.src_sent, args.eng_tok_path,
+                        args.ger_tok_path, args.dict_vars_path)
     predict.execute()
-
--- bin/train_mt.py
+++ bin/train_mt.py
@@ -5,23 +5,54 @@ Created on Thu Jun 14 22:50:48 2018
 @author: Armaan Puri
 """
 from mactrans.train_mod import trainer
-import argparse 
+import argparse
+
 
 def parser_creator():
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument('-p',meta_var="model",dest="model_path",help="Path where the model will be stored in hdf5 format post training.",type=str)
-    parser.add_argument("-c",meta_var="train_test_set",dest="dataset_path",help="Path to the corpus which shall be split into train and test sets.",type=str)
-    parser.add_argument("-w",meta_var="Word vec path", dest="word_vec_path",help="Path to preprocessed word embeddings",type=str)
-    parser.add_argument("-n",meta_var="no of epochs",dest="epochs", help='No of times whole dataset is passed through the model', type=int)
-    parser.add_argument("-b",metavar="batch_size",dest="batch_size",default=125, help="No of rows expected as output from generator in single call", type=int)
-    parser.add_argument("-s",metavar='steps_per_epoch',dest="steps_per_epoch",default=1080,help='No_of_exampleRows(train)/batch_size', type=int)
-    parser.add_argument("-v",metavar='validation_steps',dest="val_steps",default=128,help='No_of_exampleRows(test)/batch_size', type=int)
-    parser.add_argument("-N",metavar="LSTM units",dest="n_units",default=512,help="Size of the network by specifying no of LSTM units",type=int)
+
+    parser.add_argument('-p', meta_var="model", dest="model_path",
+                        help="Path to store hdf5 model file",
+                        type=str)
+
+    parser.add_argument("-c", meta_var="train_test_set", dest="dataset_path",
+                        help="Path to the cleaned corpus",
+                        type=str)
+
+    parser.add_argument("-w", meta_var="Word vec path", dest="word_vec_path",
+                        help="Path to preprocessed word embeddings",
+                        type=str)
+
+    parser.add_argument("-n", meta_var="no of epochs", dest="epochs",
+                        help='Iterations whole dataset is passed to the model',
+                        type=int)
+
+    parser.add_argument("-b", metavar="batch_size", dest="batch_size",
+                        default=125,
+                        help="No of rows output from generator in single call",
+                        type=int)
+
+    parser.add_argument("-s", metavar='steps_per_epoch',
+                        dest="steps_per_epoch",
+                        default=1080,
+                        help='No_of_exampleRows(train)/batch_size',
+                        type=int)
+
+    parser.add_argument("-v", metavar='validation_steps', dest="val_steps",
+                        default=128,
+                        help='No_of_exampleRows(test)/batch_size',
+                        type=int)
+
+    parser.add_argument("-N", metavar="LSTM units", dest="n_units",
+                        default=512,
+                        help="Size of the network (no of LSTM units)",
+                        type=int)
     return parser
 
-if __name__=="__main__":
-    parser = parser_creator()    
+if __name__ == "__main__":
+    parser = parser_creator()
     args = parser.parse_args()
-    training_model = trainer(args.model_path,args.word_vec_path,args.dataset_path)
-    training_model.execute(args.n_units,args.batch_size,args.epochs,args.steps_per_epoch,args.val_steps)
+    training_model = trainer(args.model_path, args.word_vec_path,
+                             args.dataset_path)
+    training_model.execute(args.n_units, args.batch_size,
+                           args.epochs, args.steps_per_epoch, args.val_steps)
